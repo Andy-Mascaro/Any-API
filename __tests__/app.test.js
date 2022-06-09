83d4483fca3548bfca743d4ca4523bce20a00b6b
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-06-08 17:54:55 -0700

    added to test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,13 +2,31 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const { Movie } = require('../sql/setup.sql');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('/movie should return a list of movies', async () => {
+    const res = await request(app).get('/movie');
+    const expected = Movie.map((film) => {
+      return { id: film.id, name: film.name };
+    });
+    expect(res.body).toEqual(expected);
+  });
+
+  it('/movie/:id should return movie detail', async () => {
+    const res = await request(app).get('/movie/1');
+    const mulan = {
+      id: '1',
+      name: 'Mulan',
+      type: 'Animated',
+      year: 1998,
+      rank: 10,
+    };
+    expect(res.body).toEqual(mulan);
   });
   afterAll(() => {
     pool.end();
